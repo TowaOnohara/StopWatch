60d262a955e438e5f45e5007692ee3b5dbd44d3c
Yasuo Okamoto <jack.jilljp2@gmail.com>
2018-11-04 17:19:17 +0900

    iOSプロジェクトの追加

---
+++ StopWatchIOS/StopWatchIOS/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using StopWatchCore.Models;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace StopWatchIOS
+{
+    public partial class App : Application, IModelPool
+    {
+        public StopWatchModel StopWatch { get; private set; }
+
+        public App()
+        {
+            InitializeComponent();
+            StopWatch = new StopWatchModel();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ StopWatchIOS/StopWatchIOS/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using StopWatchCore.Models;
+using StopWatchCore.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace StopWatchIOS
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var _viewModel = new StopWatchViewModel(App.Current as IModelPool);
+            this.BindingContext = _viewModel;
+
+        }
+    }
+}
